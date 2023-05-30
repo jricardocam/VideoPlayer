dc5267fcda3566e95e4bcb68ece84dfd36d754bc
jricardocam <jricardocam@gmail.com>
2023-05-29 19:10:44 -0600

    Añadiendo archivos

---
+++ video-player-base/index.js
@@ -0,0 +1,44 @@
+//Controles de Video
+console.log("linkeado");
+
+const $video = document.querySelector('#video');
+const $play = document.querySelector('#play');
+const $pause = document.querySelector('#pause');
+
+//Adelantar y Atrasar 5 segundos
+const $backward = document.querySelector('#backward');
+const $forward = document.querySelector('#forward');
+
+$play.addEventListener('click', handlePlay);
+$pause.addEventListener('click', handlePause);
+
+$backward.addEventListener('click', handleBackward);
+$forward.addEventListener('click', handleForward);
+
+
+function handlePlay() {
+    console.log("le diste click al boton de play");
+    $video.play();
+    $play.hidden = true;
+    $pause.hidden = false;
+}
+
+function handlePause() {
+    console.log("le diste click al boton de pause");
+    $video.pause();
+    $pause.hidden = true;
+    $play.hidden = false;
+}
+
+function handleBackward() {
+    console.log("le diste click al boton de backward");
+    $video.currentTime -= 5;
+}
+
+function handleForward() {
+    console.log("le diste click al boton de forward");
+    $video.currentTime += 5;
+}
+
+
+
